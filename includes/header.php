c20d74753eb8fab35d504f965b4a959aa806529b
Richard Skill <richardcskill@gmail.com>
2015-11-13 12:09:25 -0500

    project pages

---
+++ includes/header.php
@@ -0,0 +1,47 @@
+
+
+  <div class="navbar-fixed">
+    <ul id="dropdown1" class="dropdown-content">
+  <li><a href="blog.html">Blog</a></li>
+  <li class="divider"></li>
+  <li><a href="adventure.html">Adventure</a></li>
+  <li><a href="chinese.html">Chinese</a></li>
+</ul>
+    <nav class="light-blue lighten-1" role="navigation">
+      <div class="nav-wrapper container"><a id="logo-container" href="index.html" class="brand-logo">Richard Skill</a>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="about.html">About Me</a></li>
+          <li><a href="projects.html">Projects</a></li>
+          <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Blog<i class="material-icons right">arrow_drop_down</i></a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+        <ul id="nav-mobile" class="side-nav">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About Me</a></li>
+          <li><a href="projects.html">Projects</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+<!-- End Navbar -->
+
+<!-- Social Button -->
+
+<div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+    <a class="btn-floating btn-large red">
+      <i class="large material-icons">person_add</i>
+    </a>
+    <ul>
+      <li><a class="btn-floating light-blue accent-3" href="http://twitter.com/richardcskill"><img src="images/social/twitterwhite.png" class="img"></img></a></li>
+      <li><a class="btn-floating yellow darken-1"><i class="material-icons">linkedin</i></a></li>
+      <li><a class="btn-floating green"><i class="material-icons">facebook</i></a></li>
+      <li><a class="btn-floating blue"><i class="material-icons">email</i></a></li>
+    </ul>
+  </div>
+
+
+<!-- End Social Button -->
